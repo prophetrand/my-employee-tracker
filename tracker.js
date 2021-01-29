1b5c5bae55f5e89948d8a5ac0d7134d5450c574f
Rand Hale <randluca@gmail.com>
2021-01-28 20:23:01 -0800

    begin inquirer function tree

--- tracker.js
+++ tracker.js
@@ -20,5 +20,52 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     
-    // FUNCTION
+    // FUNCTIONS
+    choiceTime();
 });
+
+function choiceTime() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View all crewmates", "View crewmates by role", "View crewmates by department", "Add crewmate", "Add role", "Add department", "Update employee roles"
+            // Bonus questions: Update employee managers
+            // View crewmates by manager
+            // Delete departments, roles, and employees
+            // View the total utilized budget of a department -- ie the combined salaries of all employees in that department
+        ]
+    }).then(function(answer) {
+        switch (answer.action){
+        case "View all crewmates":
+            viewCrew();
+            break;
+
+        case "View cremates by role":
+            viewRole();
+            break;
+
+        case "View crewmates by department":
+            viewDepartment();
+            break;
+    
+        case "Add crewmate":
+            addCrewmate();
+            break;
+        
+        case "Add role":
+            addRole();
+            break;
+        
+        case "Add department":
+            addDepartment();
+            break;
+
+        case "Update employee roles":
+            updateRole();
+            break;
+        }
+        
+    });
+}
