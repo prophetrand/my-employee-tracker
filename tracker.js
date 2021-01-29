19771d6e0ce6ac01ab2e5bb1243d5cee24d88f56
Rand Hale <randluca@gmail.com>
2021-01-28 16:54:31 -0800

    require all node modules, and add console.table package

---
+++ tracker.js
@@ -0,0 +1,25 @@
+const cTable = require('console.table');
+// https://www.npmjs.com/package/console.table
+
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "password",
+    database: "#"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    
+    // FUNCTION
+});
